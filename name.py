c758069379b39e05c3e024800470cb0eeb75f4c5
wanglou28 <blinda.leo@gmail.com>
2020-03-02 19:04:48 +0800

    test page

---
+++ name.py
@@ -0,0 +1,10 @@
+name = "fuck you mother"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "wang "
+last_name = "楼"
+full_name = first_name+""+last_name
+message = "Hello，"+full_name.title()+"!"
+print(message.rstrip)
